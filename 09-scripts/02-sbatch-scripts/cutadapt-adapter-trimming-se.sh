ec367eafe9a5d718d53c69b9cc13556b0f5fcb95
Emrah Kırdök <emrahkirdok@gmail.com>
2024-10-18 17:01:21 +0300

    adapter trimming

---
+++ 09-scripts/02-sbatch-scripts/cutadapt-adapter-trimming-se.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -J cutadapt-trimming
+
+#SBATCH --partition=barbun
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=00:30:00
+
+#SBATCH -o logs/%x-%j-%N-%u.out
+#SBATCH -e logs/%x-%j-%N-%u.err
+
+# activate conda 
+eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
+
+# activate conda environment
+conda activate quality-control
+
+mkdir -p results/processed
+
+cutadapt -a AGATCGGAAGAG --minimum-length 30 -j 4 -o results/processed/SRR7029604.fastq.gz data/SRR7029604.fastq.gz
+
+mkdir -p results/fastqc-after-trimming
+
+fastqc results/processed/SRR7029604.fastq.gz --thread 4 --nogroup --outdir results/fastqc-after-trimming
+
+
