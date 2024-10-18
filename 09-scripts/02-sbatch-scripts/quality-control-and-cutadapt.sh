d2d6d19298a25dc29a62e65d46d2f3004df9358c
Emrah Kırdök <emrahkirdok@gmail.com>
2024-10-18 18:29:01 +0300

    alignment tutorial

---
+++ 09-scripts/02-sbatch-scripts/quality-control-and-cutadapt.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH -J quality-control-and-cutadapt
+
+#SBATCH --partition=barbun
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=01:00:00
+
+#SBATCH -o logs/%x-%j-%N-%u.out
+#SBATCH -e logs/%x-%j-%N-%u.err
+
+# activate conda 
+eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
+
+# activate conda environment
+conda activate quality-control
+
+mkdir -p results/processed
+
+# this time let's create a parameter
+# with this way we can reuse our scripts on different samples
+
+ID=ERR3079326
+
+mkdir -p results/fastqc-before-trimming
+
+# run fastqc on raw reads
+
+fastqc data/fastq/${ID}_1.fastq.gz data/fastq/${ID}_2.fastq.gz --thread 4 --nogroup --outdir results/fastqc-before-trimming
+
+# run cutadapt
+
+cutadapt -q 20 --minimum_length  30 --trim-n -Z -j 4 -a AGATCGGAAGAG -A AGATCGGAAGAG -o results/processed/${ID}_1.fastq.gz -p results/processed/${ID}_2.fastq.gz data/fastq/${ID}_1.fastq.gz data/fastq/${ID}_2.fastq.gz
+
+# run fastqc on processed reads
+
+mkdir -p results/fastqc-after-trimming
+
+fastqc results/processed/${ID}_1.fastq.gz results/processed/${ID}_2.fastq.gz --thread 4 --nogroup --outdir results/fastqc-after-trimming
+
+
