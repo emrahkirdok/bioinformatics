6503fe076c2a856e0f10159f48455dc2e88094f7
Emrah Kırdök <emrahkirdok@gmail.com>
2024-10-18 22:52:49 +0300

    typos

--- 09-scripts/02-sbatch-scripts/variant-calling.sh
+++ 09-scripts/02-sbatch-scripts/variant-calling.sh
@@ -18,16 +18,16 @@ conda activate alignment
 
 # create folders
 
-mkdir -p results/variant-calling
+mkdir -p results/variants
 
 # create pileup file
 
-bcftools mpileup -Ov -f data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/alignment/ERR3079326.sorted.rmdup.bam > results/alignment/ERR3079326.sorted.rmdup.likelihoods.vcf
+bcftools mpileup -Ov -f data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/alignment/ERR3079326.sorted.rmdup.bam > results/variants/ERR3079326.sorted.rmdup.likelihoods.vcf
 
 # call variants
 
-bcftools call -mv -Ov -o results/alignment/ERR3079326.sorted.rmdup.calls.vcf results/alignment/ERR3079326.sorted.rmdup.likelihoods.vcf
+bcftools call -mv -Ov -o results/variants/ERR3079326.sorted.rmdup.calls.vcf results/variants/ERR3079326.sorted.rmdup.likelihoods.vcf
 
 # this can be done easily with a oneliner, bcf is a compressed file
 
-bcftools mpileup -Ou --threads 4 -f rdata/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/alignment/ERR3079326.sorted.rmdup.bam | bcftools call -mv -Ob --threads 4 -o results/alignment/ERR3079326.sorted.rmdup.calls.bcf
+bcftools mpileup -Ou --threads 4 -f data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/alignment/ERR3079326.sorted.rmdup.bam | bcftools call -mv -Ob --threads 4 -o results/variants/ERR3079326.sorted.rmdup.calls.bcf
