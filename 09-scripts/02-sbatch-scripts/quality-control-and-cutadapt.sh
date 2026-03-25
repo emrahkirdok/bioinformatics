81fdd2263e623ac9d4ee585657988f56c0fc9238
Emrah Kırdök <emrahkirdok@gmail.com>
2026-03-25 15:29:23 +0300

    updated for singuarity

--- 09-scripts/02-sbatch-scripts/quality-control-and-cutadapt.sh
+++ 09-scripts/02-sbatch-scripts/quality-control-and-cutadapt.sh
@@ -6,20 +6,14 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 40
-#SBATCH -C weka
 #SBATCH --time=01:00:00
 
 #SBATCH -o logs/%x-%j-%N-%u.out
 #SBATCH -e logs/%x-%j-%N-%u.err
 
-# activate conda 
-eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
 
-# activate conda environment
-conda activate quality-control
-
-# this time let's create a parameter
-# with this way we can reuse our scripts on different samples
+SINGULARITY_CUTADAPT=/arf/home/egitimg14/Projects/Lectures/00-Quality-Control/containers/cutadapt\:5.0--py39hbcbf7aa_0
+SINGULARITY_FASTQC=/arf/home/egitimg14/Projects/Lectures/00-Quality-Control/containers/fastqc\:0.12.1--hdfd78af_0
 
 ID=ERR3079326
 
@@ -27,18 +21,18 @@ mkdir -p results/fastqc-before-trimming
 
 # run fastqc on raw reads
 
-fastqc data/fastq/${ID}_1.fastq.gz data/fastq/${ID}_2.fastq.gz --thread 4 --nogroup --outdir results/fastqc-before-trimming
+singularity run ${SINGULARITY_FASTQC} data/fastq/${ID}_1.fastq.gz data/fastq/${ID}_2.fastq.gz --thread 4 --nogroup --outdir results/fastqc-before-trimming
 
 # run cutadapt
 
 mkdir -p results/processed
 
-cutadapt -q 20 -m 30 --trim-n -Z -j 4 -a AGATCGGAAGAG -A AGATCGGAAGAG -o results/processed/${ID}_1.fastq.gz -p results/processed/${ID}_2.fastq.gz data/fastq/${ID}_1.fastq.gz data/fastq/${ID}_2.fastq.gz
+singularity run ${SINGULARITY_CUTADAPT} -q 20 -m 30 --trim-n -Z -j 4 -a AGATCGGAAGAG -A AGATCGGAAGAG -o results/processed/${ID}_1.fastq.gz -p results/processed/${ID}_2.fastq.gz data/fastq/${ID}_1.fastq.gz data/fastq/${ID}_2.fastq.gz
 
 # run fastqc on processed reads
 
 mkdir -p results/fastqc-after-trimming
 
-fastqc results/processed/${ID}_1.fastq.gz results/processed/${ID}_2.fastq.gz --thread 4 --nogroup --outdir results/fastqc-after-trimming
+singularity run ${SINGULARITY_FASTQC} fastqc results/processed/${ID}_1.fastq.gz results/processed/${ID}_2.fastq.gz --thread 4 --nogroup --outdir results/fastqc-after-trimming
 
 
