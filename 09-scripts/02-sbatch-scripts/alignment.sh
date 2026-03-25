1ddd80d60f717691e61574d78cec09a1936a2325
Emrah Kırdök <emrahkirdok@gmail.com>
2026-03-25 15:51:29 +0300

    script modified for singularty

--- 09-scripts/02-sbatch-scripts/alignment.sh
+++ 09-scripts/02-sbatch-scripts/alignment.sh
@@ -6,42 +6,37 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 40
-#SBATCH -C weka
 #SBATCH --time=01:00:00
 
 #SBATCH -o logs/%x-%j-%N-%u.out
 #SBATCH -e logs/%x-%j-%N-%u.err
 
-# activate conda 
-eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
-
-# activate environment
-
-conda activate alignment
-
-# create folders
 
 mkdir -p results/alignment
 
+# containers
+SINGULARITY_BWA=/arf/home/egitimg14/Lectures/01-Alignment/containers/bwa_latest.sif
+SINGULARITY_SAMTOOLS=/arf/home/egitimg14/Lectures/01-Alignment/containers/samtools_1.21.sif
+
 # align reads, first create index files
 
-bwa aln -t 4 data/ref/GCF_000014205.1_ASM1420v1_genomic.fna data/fastq/ERR3079326_1.fastq.gz > results/alignment/ERR3079326_1.sai
-bwa aln -t 4 data/ref/GCF_000014205.1_ASM1420v1_genomic.fna data/fastq/ERR3079326_2.fastq.gz > results/alignment/ERR3079326_2.sai
+singularity run ${SINGULARITY_BWA} bwa aln -t 4 data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/processed/ERR3079326_1.fastq.gz > results/alignment/ERR3079326_1.sai
+singularity run ${SINGULARITY_BWA} bwa aln -t 4 data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/processed/ERR3079326_2.fastq.gz > results/alignment/ERR3079326_2.sai
 
 # now create sam file
 
-bwa sampe data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/alignment/ERR3079326_1.sai results/alignment/ERR3079326_2.sai data/fastq/ERR3079326_1.fastq.gz data/fastq/ERR3079326_2.fastq.gz > results/alignment/ERR3079326.sam
+singularity run ${SINGULARITY_BWA} bwa sampe data/ref/GCF_000014205.1_ASM1420v1_genomic.fna results/alignment/ERR3079326_1.sai results/alignment/ERR3079326_2.sai results/processed/ERR3079326_1.fastq.gz results/processed/ERR3079326_2.fastq.gz > results/alignment/ERR3079326.sam
 
 # remove unaligned reads and create bam file
 
-samtools view -Sb -F4 results/alignment/ERR3079326.sam > results/alignment/ERR3079326.bam
+singularity run ${SINGULARITY_SAMTOOLS} samtools view -Sb -F4 results/alignment/ERR3079326.sam > results/alignment/ERR3079326.bam
 
 # sort bam file and index it for fast process
 
-samtools sort results/alignment/ERR3079326.bam -o results/alignment/ERR3079326.sorted.bam
-samtools index results/alignment/ERR3079326.sorted.bam
+singularity run ${SINGULARITY_SAMTOOLS} samtools sort results/alignment/ERR3079326.bam -o results/alignment/ERR3079326.sorted.bam
+singularity run ${SINGULARITY_SAMTOOLS}samtools index results/alignment/ERR3079326.sorted.bam
 
 # remove optical pcr duplicates and index
 
-samtools rmdup results/alignment/ERR3079326.sorted.bam results/alignment/ERR3079326.sorted.rmdup.bam
-samtools index results/alignment/ERR3079326.sorted.rmdup.bam
+singularity run ${SINGULARITY_SAMTOOLS} samtools rmdup results/alignment/ERR3079326.sorted.bam results/alignment/ERR3079326.sorted.rmdup.bam
+singularity run ${SINGULARITY_SAMTOOLS} samtools index results/alignment/ERR3079326.sorted.rmdup.bam
